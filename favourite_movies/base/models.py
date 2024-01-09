83ebf81eeb7781af3ab4b2171c24f2961972b293
mikheil <uglavamisho@gmail.com>
2024-01-09 00:26:36 -0500

    Initial commit

---
+++ favourite_movies/base/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Movie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    release_year = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    imdb_rating = models.CharField(max_length=10, blank=True, default='0.0')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created_at']
---
+++ favourite_movies/base/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from . import views
+
+
+urlpatterns = [
+    path('', views.MoviesList.as_view(), name='movies-list'),
+
+    path('login/', views.CustomLogin.as_view(), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
+
+    path('add-movie/', views.AddMovie.as_view(), name='add-movie'),
+    path('detail-movie/<int:pk>/', views.MovieDetail.as_view(), name='detail-movie'),
+    path('delete-movie/<int:pk>/', views.MovieDelete.as_view(), name='delete-movie'),
+    path('update-movie/<int:pk>/', views.MovieDelete.as_view(), name='update-movie'),
+]
