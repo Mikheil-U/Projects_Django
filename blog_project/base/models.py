ca53c718807452fbd17151e1d3ee9a04ed6388c9
mikheil <uglavamisho@gmail.com>
2024-02-01 00:55:48 -0500

    update the Post model

--- blog_project/base/models.py
+++ blog_project/base/models.py
@@ -9,3 +9,7 @@ class Post(models.Model):
     # we didn't use auto_now__add because we won't be able to update the time if used that function
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
