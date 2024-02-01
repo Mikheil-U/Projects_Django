cdea3b7265c98d280fbd4f934241747084afc818
mikheil <uglavamisho@gmail.com>
2024-02-01 00:49:57 -0500

    create a model for Post

--- blog_project/base/models.py
+++ blog_project/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    # we didn't use auto_now__add because we won't be able to update the time if used that function
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
