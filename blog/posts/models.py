31afb13873330a85486f1f43004062a33273fc59
mikheil <uglavamisho@gmail.com>
2024-02-13 00:37:02 -0500

    Add PostListView, PostDetailView

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from blog.groups.models import Group
+from django.contrib.auth import get_user_model
+import misaka
 
-# Create your models here.
+User = get_user_model()  # Gets the current logged-in user
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message_html)
+        super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single', kwargs={'username': self.user.username, 'pk':self.pk})
+
+    class Meta:
+        ordering = ['-created']
+        unique_together = ('user', 'message')
--- blog_project/base/views.py
+++ blog_project/base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 def home(request):
     return render(request, 'base/home.html', {'posts': Post.objects.all()})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'base/home.html'
+    context_object_name = 'posts'  # same as {'posts': Post.objects.all()}
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
