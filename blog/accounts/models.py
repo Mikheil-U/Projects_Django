0ef0bf2c85105aba01eef914bb20ac2c43bc2eed
Mikheil-U <124485283+Mikheil-U@users.noreply.github.com>
2024-02-22 15:10:05 -0500

    Delete blog directory
    Remove and reupload newer version

--- blog/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib import auth
-
-
-class User(auth.models.User, auth.models.PermissionsMixin):
-
-    def __str__(self):
-        return f'@{self.username}'  # username is an attribute of User class
