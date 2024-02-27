92014904eb076d759c448e79a23ce61f9eb05281
Mikheil-U <uglavamisho@gmail.com>
2024-02-27 00:58:15 -0500

    Add category and product models

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}  # pre-populate the slug field
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
