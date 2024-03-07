07a8b06ffd5a58d0179cff1713c297560328d23c
Mikheil-U <uglavamisho@gmail.com>
2024-03-07 00:37:22 -0500

    Add data grid for products.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from . models import Category
+from .models import Category, Product
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    return render(request, 'store/store.html', {'all_products': Product.objects.all()})
 
 
 def categories(request):
