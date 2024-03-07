bc4addb6a22a0c6f1089bc033efd42d4bb1383e1
Mikheil-U <uglavamisho@gmail.com>
2024-03-07 00:26:25 -0500

    Add categories to the navbar dropdown menu.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . models import Category
 
 
 def store(request):
     return render(request, 'store/store.html')
+
+
+def categories(request):
+
+    return {'all_categories': Category.objects.all()}
