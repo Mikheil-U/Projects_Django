3a8899f7eebd1e4cb2d78ee1ce7ffcf1f2486465
Mikheil-U <uglavamisho@gmail.com>
2024-04-09 21:36:54 -0400

    Add - search todos

--- todo_app/base/views.py
+++ todo_app/base/views.py
@@ -17,10 +17,19 @@ from .models import Todo
 class TodoListView(LoginRequiredMixin, ListView):
     model = Todo
     template_name = 'base/todo_list.html'
-    context_object_name = 'todo_list'
+    context_object_name = 'todos'
 
     def get_queryset(self, *args, **kwargs):
         return Todo.objects.filter(user=self.request.user).order_by('completed')  # user specific data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['todos'] = context['todos'].filter(title__icontains=search_input)
+
+        return context
 
 
 class CreateTodo(LoginRequiredMixin, CreateView):
