b8da9cfb11b327ce6dad52b2a7159f61a85ee021
Mikheil-U <uglavamisho@gmail.com>
2024-05-29 00:15:23 -0400

    Update models change date field

--- favourite_movies/base/models.py
+++ favourite_movies/base/models.py
@@ -17,9 +17,13 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     genre = models.CharField(max_length=250, blank=True, null=True)
     director = models.CharField(max_length=250, blank=True, null=True)
-    release_year = models.DateField(null=True, blank=True)
-    imdb_rating = models.IntegerField(null=True, blank=True)
+    release_year = models.CharField(null=True, blank=True, max_length=20)
+    imdb_rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    watched = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-watched']
 
     def __str__(self):
         return self.title
