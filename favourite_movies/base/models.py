38dba93a1716133107c657c57a16e77e2c495ec4
Mikheil-U <uglavamisho@gmail.com>
2024-05-28 21:18:29 -0400

    Modify models, change datetimefield to datefield

--- favourite_movies/base/models.py
+++ favourite_movies/base/models.py
@@ -17,7 +17,7 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     genre = models.CharField(max_length=250, blank=True, null=True)
     director = models.CharField(max_length=250, blank=True, null=True)
-    release_year = models.DateTimeField(null=True, blank=True)
+    release_year = models.DateField(null=True, blank=True)
     imdb_rating = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
