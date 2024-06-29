993d30f7590d916e81a6464be2a609a24e7fbfe9
Mikheil-U <uglavamisho@yahoo.com>
2024-06-29 15:18:00 +0400

    Sort lists by date

--- favourite_movies/base/models.py
+++ favourite_movies/base/models.py
@@ -8,6 +8,9 @@ class Playlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movies = models.ManyToManyField('Movie', related_name='playlists')
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return self.title
 
