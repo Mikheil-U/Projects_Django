75ada7fb34c0ab041b1e9dc37e313368b1a7c28d
Mikheil-U <uglavamisho@gmail.com>
2024-08-17 13:22:26 -0400

    Fix - logout functionality

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
+    path('logout-user/', views.logout_user, name='logout'),
 ]
