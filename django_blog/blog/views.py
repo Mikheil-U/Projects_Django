4609cae9a299d6e8910b6ad494cada5ad40bcf2d
Mikheil-U <uglavamisho@gmail.com>
2025-03-16 15:36:40 -0400

    Add basic templates for home page and about page, and add navbar and side bar.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/home.html', {})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
