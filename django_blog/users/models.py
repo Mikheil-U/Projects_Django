c6bed3708859fda0b0f3145ab5627ecdf6373dd1
Mikheil-U <uglavamisho@gmail.com>
2025-03-18 18:30:18 -0400

    Fix image uploads

--- django_blog/users/models.py
+++ django_blog/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,3 +10,15 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    def save(self):
+        """
+        Overrides the save method to set the image size manually
+        """
+        super().save()
+        # resize the image
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
