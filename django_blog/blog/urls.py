203478e3ba7a25c31e1a7a9df07a127dfa71439f
Mikheil-U <uglavamisho@gmail.com>
2025-03-19 19:50:06 -0400

    Implement CRUD for model Post.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post-create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post-update/<int:pk>/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post-delete/<int:pk>/', views.PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='about'),
 ]
